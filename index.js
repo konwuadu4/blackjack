6ce0ebb5209ebeae6bc01c3592e11bd9e5fb709f
konwuadu4 <konwuadu@gmail.com>
2022-05-10 21:49:04 -0400

    random card function

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let firstCard = 4
-let secondCard = 10
-let cards = [firstCard, secondCard]
 
-let sum = firstCard + secondCard;
+let cards = []
+
+let sum = 0
 
 let hasBlackJack = false
-let isAlive = true 
+let isAlive = false 
 
 let message = " "
 
@@ -17,11 +16,44 @@ let cardsEl = document.getElementById("cards-el")
 //let sumEl = document.querySelector("#sum-el") //this is to select an id
 //let sumEl = document.querySelector(".sum-el") //this is to select a class
 
+function getRandomCard(){
+    let randomNum = Math.floor(Math.random() * 13) + 1
+    if (randomNum === 1){
+        return 11
+    } else if (randomNum > 10){
+        return 10
+    } else {
+        return randomNum
+    }
+}
+
 function startGame(){
+    isAlive = true
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+
+    sum = firstCard + secondCard
+
+    // this does not allow to restart the game
+    // for (let i = 0; i < 2; i++){
+    //     cards.push(getRandomCard())
+    //     sum += cards[i]
+    // }
+
     renderGame()
 }
 
 function renderGame() {
+    cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " ";
+    }
+
+    sumEl.textContent =   `Sum: ${sum}`
+
     if (sum <= 20) {
         message = "Do you want to draw a new card";
     } else if (sum === 21) {
@@ -31,18 +63,15 @@ function renderGame() {
         message = "You're out the game!";
         isAlive = false
     }
-    
     messageEl.textContent = message;
-    sumEl.textContent =   `Sum: ${sum}`
-    cardsEl.textContent = `Cards: ${cards[0]}   ${cards[1]}`
+
 }
 
 function newCard(){
     console.log("Drawing a new card from the deck! ");
 
-    let card = 7;
-    cards.push(card)
-
+    let card = getRandomCard();
     sum += card;
+    cards.push(card)
     renderGame()
 }
