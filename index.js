8d43508b680fde4fd39fc620ecb6991c35bceff8
konwuadu4 <konwuadu@gmail.com>
2022-05-10 22:10:46 -0400

    updated blackjack

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Dinou",
+    chips: 200
+}
 
 let cards = []
 
@@ -11,6 +15,10 @@ let message = " "
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name +  ": $" + player.chips
 
 //using css selector
 //let sumEl = document.querySelector("#sum-el") //this is to select an id
@@ -29,49 +37,52 @@ function getRandomCard(){
 
 function startGame(){
     isAlive = true
-
+    
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
-
-    sum = firstCard + secondCard
-
+    
+    sum = firstCard + secondCard;
+    
     // this does not allow to restart the game
     // for (let i = 0; i < 2; i++){
     //     cards.push(getRandomCard())
     //     sum += cards[i]
     // }
-
+    
     renderGame()
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
-
+    
     for (let i = 0; i < cards.length; i++){
         cardsEl.textContent += cards[i] + " ";
     }
-
+    
     sumEl.textContent =   `Sum: ${sum}`
-
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card";
     } else if (sum === 21) {
         message = "Woohoo! You've got Blackjack!";
-        hasBlackJack = true
+        hasBlackJack = true;
     } else if (sum > 21) {
         message = "You're out the game!";
-        isAlive = false
+        isAlive = false;
     }
     messageEl.textContent = message;
-
+    
 }
 
-function newCard(){
-    console.log("Drawing a new card from the deck! ");
 
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card)
-    renderGame()
+function newCard(){
+    // console.log("Drawing a new card from the deck! ");
+    
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card)
+        renderGame()
+    }
 }
